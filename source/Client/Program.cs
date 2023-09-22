a4a18f4a91429a7d1d3ba5f22627f8c51c453784
Günther Foidl <gue@korporal.at>
2023-09-23 00:19:02 +0200

    Demo for Unix domain sockets

--- source/Client/Program.cs
+++ source/Client/Program.cs
@@ -1,13 +1,21 @@
+#define USE_UNIX_DOMAIN_SOCKETS
+//-----------------------------------------------------------------------------
 using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
 using Grpc.Core;
 using Grpc.Net.Client;
 using MathEndpoint;
-
 Console.WriteLine("Waiting for server to start...any key to continue");
 Console.ReadKey();
 
+#if USE_UNIX_DOMAIN_SOCKETS
+string socketPath         = Path.Combine(Path.GetTempPath(), "gRPC-Test.sock");
+using GrpcChannel channel = CreateChannel(socketPath);
+#else
 //using GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:5001");
 using GrpcChannel channel = GrpcChannel.ForAddress("http://localhost:5000");
+#endif
 
 Calc.CalcClient client = new(channel);
 
@@ -25,3 +33,43 @@ catch (RpcException ex) when (!Debugger.IsAttached)
 }
 
 Console.WriteLine("\nEnd.");
+//-----------------------------------------------------------------------------
+#if USE_UNIX_DOMAIN_SOCKETS
+// Cf. https://learn.microsoft.com/en-us/aspnet/core/grpc/interprocess-uds
+
+static GrpcChannel CreateChannel(string socketPath)
+{
+    UnixDomainSocketEndPoint udsEndpoint                 = new(socketPath);
+    UnixDomainSocketsConnectionFactory connectionFactory = new(udsEndpoint);
+    SocketsHttpHandler socketsHttpHandler                = new()
+    {
+        ConnectCallback = connectionFactory.ConnectAsync
+    };
+
+    return GrpcChannel.ForAddress("http://localhost", new GrpcChannelOptions
+    {
+        HttpHandler = socketsHttpHandler
+    });
+}
+//-----------------------------------------------------------------------------
+public class UnixDomainSocketsConnectionFactory(EndPoint endPoint)
+{
+    private readonly EndPoint _endPoint = endPoint;
+    //-------------------------------------------------------------------------
+    public async ValueTask<Stream> ConnectAsync(SocketsHttpConnectionContext context, CancellationToken cancellationToken = default)
+    {
+        Socket socket = new(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified);
+
+        try
+        {
+            await socket.ConnectAsync(_endPoint, cancellationToken);
+            return new NetworkStream(socket, ownsSocket: true);
+        }
+        catch
+        {
+            socket.Dispose();
+            throw;
+        }
+    }
+}
+#endif
--- source/Server/Program.cs
+++ source/Server/Program.cs
@@ -1,4 +1,5 @@
 //#define USE_CUSTOM_KESTREL_PORT
+#define USE_UNIX_DOMAIN_SOCKETS
 //-----------------------------------------------------------------------------
 using Server;
 using Server.Math;
@@ -21,6 +22,19 @@ builder.WebHost.ConfigureKestrel(options =>
     options.ListenLocalhost(50051, listenOptions =>
     {
         listenOptions.Protocols = HttpProtocols.Http2;
+    });
+});
+#elif USE_UNIX_DOMAIN_SOCKETS
+// Cf. https://learn.microsoft.com/en-us/aspnet/core/grpc/interprocess-uds
+
+string socketsPath = Path.Combine(Path.GetTempPath(), "gRPC-Test.sock");
+File.Delete(socketsPath);   // Not mandatory, but just as additional safety to prevent "address already in use" error
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenUnixSocket(socketsPath, listenOptions =>
+    {
+        listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2;
     });
 });
 #endif
